29f643d90934a02f8c7d16a43f023178b5376872
Vamsinator <vamsi.ghorakavi@utexas.edu>
2018-10-20 19:11:35 -0500

    changed pwm control

--- src/main.cpp
+++ src/main.cpp
@@ -1,77 +1,130 @@
 #include "mbed.h"
+#include "rtos.h"
 #include "FastPWM.h"
-#include "PWM.h"
-
+#include <vector>
 // Number of dutycycle steps for output wave
-#define SINE_STEPS        32
+#define SINE_STEPS        750
 // Frequency of output sine in Hz
 #define SINE_OUT_FREQ     60
 
+#define TRIANGE_FREQUENCY 12000
+
+#define TRIANGLE_STEPS  10
+
+double TRIANGLE_INCRIMENT = .1;
 // Constants to compute the sine waveform
 #define PI                 3.141592f
 #define SINE_STEPS_RAD    (2.0f * PI / (float)SINE_STEPS)
-
+#define SWM_STEPS         24
 // Table to generate the sine waveform using dutycycles
 float sine_duty[SINE_STEPS];
 
+float swm_duty[SWM_STEPS] = {0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15,
+  0.15, 0.325, 0.50, 0.675, 0.85, 0.85, 0.85, 0.85, 0.85, 0.85, 0.85, 0.85,
+  0.85, 0.675, 0.50, 0.325};
 //PWM pinout mapping
-PWMPinout pwmArray[] = {
-  PWMPinout(FastPWM(D3), 10000, 16000000),
-  PWMPinout(FastPWM(D9), 10000, 16000000),
-  PWMPinout(FastPWM(D8), 10000, 16000000),
-  PWMPinout(FastPWM(D12), 10000, 16000000),
-  PWMPinout(FastPWM(D6), 10000, 16000000),
-  PWMPinout(FastPWM(D10), 10000, 16000000),
-  PWMPinout(FastPWM(A0), 100000, 16000000),
-};
 
-AnalogIn DCCurrentIn (A1);
-AnalogIn DCVoltageIn (A2);
+Serial pc(D1, D0); // tx, rx
+
+
+
+
+DigitalOut AP(D3);
+DigitalOut AN(D9);
+DigitalOut BP(D8);
+DigitalOut BN(D12);
+DigitalOut CP(D6);
+DigitalOut CN(D7);
+FastPWM Boost(A0);
+
+
+AnalogIn DCCurrentIn (A2);
+AnalogIn DCVoltageIn (A5);
 //Heartbeat LED
 DigitalOut myled(LED1);
-
+Thread thread;
+Thread thread2;
+Thread thread3;
 //Ticker to update the PWM dutycycle
 Ticker pwm_ticker;
 Ticker pwm_tickerN;
 Ticker boost_ticker;
 
+int sine_index = 0;
 // Ticker calls this fucntion to update the PWM dutycycle
 void pwm_duty_updater() {
-  static int idx=0;
-  static int idx2=10;
-  static int idx3=22;
+  static double counter = 0.0f;
+  static int sine_index = 0;
+  vector <DigitalOut> toZero;
+  vector <DigitalOut> toOne;
+  if (sine_duty[sine_index] >= counter)
+  {
+    if(AP == 0) {
+      toZero.push_back(AN);
+      toOne.push_back(AP);
+    }
+  }
+  else {
+    if(AP == 1) {
+      toZero.push_back(AP);
+      toOne.push_back(AN);
+    }
+  }
 
-  pwmArray[3].SetDuty(sine_duty[idx]);  // Set the dutycycle % to next value in array
-  pwmArray[4].SetDuty(sine_duty[idx2]);  // Set the dutycycle % to next value in array
-  pwmArray[5].SetDuty(sine_duty[idx3]);  // Set the dutycycle % to next value in array
+  if (sine_duty[(sine_index + SINE_STEPS/3)%SINE_STEPS] >= counter)
+  {
+    if(BP == 0) {
+      toZero.push_back(BN);
+      toOne.push_back(BP);
+    }
+  }
+  else {
+    if(BP == 1) {
+      toZero.push_back(BP);
+      toOne.push_back(BN);
+    }
+  }
 
-  idx++; idx2++; idx3++;                        // Increment the idx
-  if (idx == SINE_STEPS) idx=0;  // Reset the idx when teh end has been reached
-  if (idx2 == SINE_STEPS) idx2=0;
-  if (idx3 == SINE_STEPS) idx3=0;
+  if (sine_duty[(sine_index + 2*SINE_STEPS/3)%SINE_STEPS] >= counter)
+  {
+    if(CP == 0) {
+      toZero.push_back(CN);
+      toOne.push_back(CP);
+    }
+  }
+  else {
+    if(CP == 1) {
+      toZero.push_back(CP);
+      toOne.push_back(CN);
+    }
+  }
 
+  for (int i = 0; i < toZero.size(); i++) {
+    toZero[i] = 0;
+  }
+  wait_us(1);
+  for (int i = 0; i < toOne.size(); i++) {
+    toOne[i] = 1;
+  }
+
+
+  sine_index++;
+  counter += TRIANGLE_INCRIMENT;
+
+  if (counter <= -1.0f || counter >= 1.0f) {
+    TRIANGLE_INCRIMENT *= -1;
+  }
+  if (sine_index >= SINE_STEPS) {
+    sine_index = 0;
+  }
 }
 
-void pwm_duty_updaterN() {
-  static int idx=0;
-  static int idx2=10;
-  static int idx3=22;
 
-  pwmArray[0].SetDuty(sine_duty[SINE_STEPS-idx-1]);  // Set the dutycycle % to next value in array
-  pwmArray[1].SetDuty(sine_duty[SINE_STEPS-idx2-1]);  // Set the dutycycle % to next value in array
-  pwmArray[2].SetDuty(sine_duty[SINE_STEPS-idx3-1]);  // Set the dutycycle % to next value in array
-
-  idx++; idx2++; idx3++;                        // Increment the idx
-  if (idx == SINE_STEPS) idx=0;  // Reset the idx when teh end has been reached
-  if (idx2 == SINE_STEPS) idx2=0;
-  if (idx3 == SINE_STEPS) idx3=0;
-
-}
 double getPanelVoltage(){
-  return DCVoltageIn.read();
+  return DCVoltageIn.read()*3.3f*16;
 }
 double getPanelCurrent() {
-  return DCCurrentIn.read();
+  return DCCurrentIn.read()*3.3f*1.6f;
 
 }
 
@@ -101,13 +154,13 @@ double calculateDutyCycle()
   return dutyCycle;
 
   */
-  return 0.5;
+  return 0.2;
 }
 
 void boostUpdater()
 {
-  //static double dutyCycle = calculateDutyCycle();
-  pwmArray[6].SetDuty(0.2);
+  static double dutyCycle = calculateDutyCycle();
+  Boost.write(dutyCycle);
 }
 
 
@@ -117,21 +170,26 @@ int main() {
 
   // Init the duty cycle array
   for (i=0; i<SINE_STEPS; i++) {
-    sine_duty[i] = ( sin(i * SINE_STEPS_RAD) + 1.0f ) / 2.0f;  // convert sine (-1.0 .. +1.0) into dutycycle (0.0 .. 1.0)
+    sine_duty[i] = sin(i * SINE_STEPS_RAD);  // convert sine (-1.0 .. +1.0) into dutycycle (0.0 .. 1.0)
   }
+
+  Boost.period(1.0f/ (10000));
 
   // Init the Ticker to call the dutycyle updater at the required interval
   // The update should be at (SINE_STEPS * SINE_OUT_FREQ)
 
+  myled = 1;
   __disable_irq();
-  pwm_ticker.attach(&pwm_duty_updater, 1.0f / (float)(SINE_STEPS * SINE_OUT_FREQ));
-  pwm_tickerN.attach(&pwm_duty_updaterN, 1.0f / (float)(SINE_STEPS * SINE_OUT_FREQ));
+  pwm_ticker.attach(&pwm_duty_updater, (1.0f / (float)(10 * TRIANGE_FREQUENCY) - 1e-6f));
   boost_ticker.attach(&boostUpdater, 2);
   __enable_irq();
 
+
   while(1){ //infinite loop
-    myled = !myled;
+    myled = myled^1;
     wait(0.5);
+
+
   }
 
 }
