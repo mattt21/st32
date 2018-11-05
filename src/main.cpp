65896939b4ba479e09d7d033e88c678fef5266db
Vamsinator <vamsi.ghorakavi@utexas.edu>
2018-11-05 15:42:00 -0600

    Refactored Code and Ready to merge with bluetooth code

--- src/main.cpp
+++ src/main.cpp
@@ -1,109 +1,20 @@
 #include "mbed.h"
 #include "rtos.h"
 #include "FastPWM.h"
-#include "PWM.h"
 #include <string>
 #include <vector>
 #include "bleopcode.h"
-//#include "Inverter.h"
+#include "Inverter.h"
 
-double TRIANGLE_INCRIMENT = .3;
-double  SINE_OUT_FREQ   =  60;
-
-Ticker pwm_ticker;
 
 Serial pc(D1, D0); //tx, rx
-DigitalOut AP(D3);
-DigitalOut AN(D9);
-DigitalOut BP(D8);
-DigitalOut BN(D12);
-DigitalOut CP(D6);
-DigitalOut CN(D7);
+
 DigitalOut out (D14);
-// Number of elements in sampled sine wave
-#define SINE_STEPS        400
-// Frequency of output triangle in Hz
-#define TRIANGE_FREQUENCY 500
-// Number of steps per each Triangle Wave
-#define TRIANGLE_STEPS  10
-
-// Constants to compute the sine waveform
-#define PI                 3.141592f
-#define SINE_STEPS_RAD    (2.0f * PI / (float)SINE_STEPS)
-#define SWM_STEPS         24
-// Table to generate the sine waveform using dutycycles
-float sine_duty[SINE_STEPS];
 
 
 
-// Ticker calls this fucntion to update the PWM dutycycle
-void pwm_duty_updater() {
-  // counter is the triangle wave to compare the sine to
-  static double counter = 0.0f;
-  static int sine_index = 0;
-  // Two vectors for the pins to open and close
-  vector <DigitalOut> toZero;
-  vector <DigitalOut> toOne;
-  // Comparing the values for each PWM pin
-  if (sine_duty[sine_index] >= counter && AP == 0) {
-      toZero.push_back(AN);
-      toOne.push_back(AP);
-  }
-  else if(AP == 1) {
-      toZero.push_back(AP);
-      toOne.push_back(AN);
-  }
-
-  if (sine_duty[(sine_index + SINE_STEPS/3)%SINE_STEPS] >= counter && BP == 0) {
-      toZero.push_back(BN);
-      toOne.push_back(BP);
-  }
-  else if(BP == 1) {
-      toZero.push_back(BP);
-      toOne.push_back(BN);
-  }
-
-  if (sine_duty[(sine_index + 2*SINE_STEPS/3)%SINE_STEPS] >= counter && CP == 0) {
-      toZero.push_back(CN);
-      toOne.push_back(CP);
-  }
-  else if(CP == 1) {
-      toZero.push_back(CP);
-      toOne.push_back(CN);
-  }
-
-  // Set the correct pins to zero
-  for (int i = 0; i < toZero.size(); i++) {
-    toZero[i] = 0;
-  }
-  // Wait the dead Time. I approximated it as 1000 nanoseconds or 1 microsecond
-  wait_us(1);
-  for (int i = 0; i < toOne.size(); i++) {
-    toOne[i] = 1;
-  }
 
 
-  // Incriment the sine index and the counter
-  sine_index++;
-  counter += TRIANGLE_INCRIMENT;
-
-  if (counter <= -1.5f || counter >= 1.5f) {
-    TRIANGLE_INCRIMENT *= -1;
-  }
-  if (sine_index >= SINE_STEPS) {
-    sine_index = 0;
-  }
-}
-void initInverter() {
-  int i;
-  // Generate Sine Wave
-  for (i=0; i<SINE_STEPS; i++) {
-    sine_duty[i] = sin(i * SINE_STEPS_RAD);
-  }
-  //Attach Intterupt
-  pwm_ticker.attach(&pwm_duty_updater, (1.0f / (float)(10.0f * TRIANGE_FREQUENCY)));
-
-}
 /*
   device HM-10
   HM10 - STM32
@@ -192,16 +103,6 @@ void blePushData(std::vector<char> &bleData){
   EvalCode(bleData);
 }
 
-void changeMotorFrequency(double freq) {
-  int required_freq = freq;
-  if(freq>60.0f) {
-    required_freq = 60;
-  }
-
-  pwm_ticker.detach();
-  pwm_ticker.attach(&pwm_duty_updater, 1.0f/(10.0f*40.0f*required_freq));
-}
-
 volatile bool newData = false;
 volatile float inputs[3];
 
@@ -244,10 +145,11 @@ int main() {
 
   myled = 1;
   initInverter();
+  /*
   __disable_irq();
-  //boost_ticker.attach(&boostUpdater, 2);
+  boost_ticker.attach(&boostUpdater, 2);
   __enable_irq();
-
+  */
   out = 1;
   while(1){ //infinite loop
     if(newData) {
